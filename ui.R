c2ba320634db7c70cc3dd240592135878c0a53a5
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2017-05-30 16:48:11 -0700

    changed ui.r select inputs

--- ui.R
+++ ui.R
@@ -18,9 +18,15 @@ shinyUI(navbarPage('Terrorism over Time',
                                             selected = "All"),
                                 sliderInput("YearsMap", label = h3("Time Range"), sep = "", min = 1970, 
                                             max = 2015, value = c(1970, 2015)),
-                                selectInput("TypeMap", label = h3("Select based on Attack Type, Target, or Weapon Type"), 
-                                            choices = list( "None", "Target Type", "Attack Type", "Weapon Type"),
-                                                  selected = "None")
+                                selectInput("TargetTypeMap", label = h3("Select based on Target Type"), 
+                                            choices = list("All", "Business","Government","Police","Military","Abortion Related", "Airports & Aircraft", "Government(Diplomatic)", "Educational Institution", "Food or Water Supply", "Journalist & Media", "Maritime", "NGO", "Other", "Private Citizens & Property"), 
+                                            selected = "All"),
+                                selectInput("AttackTypeMap", label = h3("Select based on Attack Type"), 
+                                            choices = list("All", "Assassination", "Armed Assault", "Bombing/Explosion", "Hijacking", "Hostage Taking (Barricade Incident)", "Hostage Taking (Kidnapping)", "Facility/Infrastructure Attack", "Unarmed Assault", "Unknown"), 
+                                            selected = "All"),
+                                selectInput("WeaponTypeMap", label = h3("Select based on Weapon Type"), 
+                                            choices = list("All", "Biological", "Chemical", "Radiological", "Nuclear", "Firearms", "Explosives/Bombs/Dynamite", "Fake Weapons", "Incendiary", "Melee", "Vehicle", "Sabotage Equipment", "Other", "Unknown"), 
+                                            selected = "All")
                               ),
                               
                               # Main panel: display plotly map
@@ -75,12 +81,15 @@ shinyUI(navbarPage('Terrorism over Time',
                                             selected = "All"),
                                 sliderInput("YearsTime", label = h3("Time Range"), sep = "", min = 1970, 
                                             max = 2015, value = c(1970, 2015)),
-                                selectInput("TypeTime", label = h3("Select based on Attack Type, Target, or Weapon Type"), 
-                                            choices = list( "None", 
-                                                            "Target Type" <- list("Business"=1,"Government"=2,"Police"=3,"Military"=4,"Abortion Related"=5, "Airports & Aircraft"=6, "Government(Diplomatic)"=7, "Educational Institution"=8, "Food or Water Supply"=9, "Journalist & Media"=10, "Maritime"=11, "NGO"=12, "Other"=13, "Private Citizens & Property"=14), 
-                                                            "Attack Type" <- list("Assassination"=15, "Armed Assault"=16, "Bombing/Explosion"=17, "Hijacking"=18, "Hostage Taking (Barricade Incident)"=19, "Hostage Taking (Kidnapping)"=20, "Facility/Infrastructure Attack"=21, "Unarmed Assault"=22, "Unknown"=23), 
-                                                            "Weapon Type" <- list("Biological"=24, "Chemical"=25, "Radiological"=26, "Nuclear"=27, "Firearms"=28, "Explosives/Bombs/Dynamite"=29, "Fake Weapons"=30, "Incendiary"=31, "Melee"=32, "Vehicle"=33, "Sabotage Equipment"=34, "Other"=35, "Unknown"=36)), 
-                                            selected = "None")
+                                selectInput("TargetTypeTime", label = h3("Select based on Target Type"), 
+                                            choices = list("All", "Business","Government","Police","Military","Abortion Related", "Airports & Aircraft", "Government(Diplomatic)", "Educational Institution", "Food or Water Supply", "Journalist & Media", "Maritime", "NGO", "Other", "Private Citizens & Property"), 
+                                            selected = "All"),
+                                selectInput("AttackTypeTime", label = h3("Select based on Attack Type"), 
+                                            choices = list("All", "Assassination", "Armed Assault", "Bombing/Explosion", "Hijacking", "Hostage Taking (Barricade Incident)", "Hostage Taking (Kidnapping)", "Facility/Infrastructure Attack", "Unarmed Assault", "Unknown"), 
+                                            selected = "All"),
+                                selectInput("WeaponTypeTime", label = h3("Select based on Weapon Type"), 
+                                            choices = list("All", "Biological", "Chemical", "Radiological", "Nuclear", "Firearms", "Explosives/Bombs/Dynamite", "Fake Weapons", "Incendiary", "Melee", "Vehicle", "Sabotage Equipment", "Other", "Unknown"), 
+                                            selected = "All")
                               ),
                               
                               # Create a main panel, to display plotly Scatter plot
